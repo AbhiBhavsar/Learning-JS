7f04841e7222a08c8874e7d2324258b332168528
Abhishek Bhavsar <abhi.n.bhavsar@gmail.com>
2020-06-19 07:49:42 +0530

    Added final changes from mongo db driver module

--- util/database.js
+++ util/database.js
@@ -1,27 +1,29 @@
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
+
 const mongoConnect = callback => {
-	MongoClient.connect(
-		"mongodb+srv://node-user:node-user@node-testing-oefoa.mongodb.net/shop?retryWrites=true&w=majority"
-	)
-		.then(client => {
-			console.log(`Connected...!`);
-			_db = client.db(); //we can pass a database name to connect to as db's parameter
-			callback();
-		})
-		.catch(err => {
-			console.log(err);
-			throw err;
-		});
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
 const getDb = () => {
-	if (_db) {
-		return _db;
-	}
-	throw "No Database Found";
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
 };
+
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
