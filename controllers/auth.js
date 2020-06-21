d0beb394887dacd44cbc8e7696ae5f76da6e6447
Abhishek Bhavsar <abhi.n.bhavsar@gmail.com>
2020-06-21 11:31:20 +0530

    Added the package for csrf attck prevention

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,72 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
-  });
+	res.render("auth/login", {
+		path: "/login",
+		pageTitle: "Login",
+		isAuthenticated: false
+	});
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
-  });
+	res.render("auth/signup", {
+		path: "/signup",
+		pageTitle: "Signup",
+		isAuthenticated: false
+	});
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+	const { email, password } = req.body;
+	User.findOne({ email: email })
+		.then(user => {
+			if (!user) {
+				return res.redirect("/login");
+			}
+			bcrypt.compare(password, user.password).then(ifMatched => {
+				if (ifMatched) {
+					req.session.isLoggedIn = true;
+					req.session.user = user;
+					return req.session.save(err => {
+						console.log(err);
+						res.redirect("/");
+					});
+				}
+				res.redirect("/login");
+			});
+		})
+		.catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+	const { email, password, confirmPassword } = req.body;
+	User.findOne({ email: email })
+		.then(userDoc => {
+			if (userDoc) {
+				res.redirect("/");
+			}
+			return bcrypt
+				.hash(password, 12)
+				.then(hashedPwd => {
+					const user = new User({
+						email: email,
+						password: hashedPwd,
+						cart: { items: [] }
+					});
+					return user.save();
+				})
+				.then(result => {
+					res.redirect("/login");
+				});
+		})
+
+		.catch(err => console.log(err));
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
-    console.log(err);
-    res.redirect('/');
-  });
+	req.session.destroy(err => {
+		console.log(err);
+		res.redirect("/");
+	});
 };
