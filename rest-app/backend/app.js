ad7aeafdb27bc7397bbbca944564593338ac5e33
Abhishek Bhavsar <abhi.n.bhavsar@gmail.com>
2020-08-20 11:07:17 +0530

    Added code to post the feed

--- rest-app/backend/app.js
+++ rest-app/backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
+
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -9,7 +10,7 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader(
 		"Access-Control-Allow-Methods",
-		"GET, POST, DELETE, PUT, PATCH"
+		"GET, POST, DELETE, PUT, PATCH, OPTIONS"
 	);
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	next();
--- rest-app/backend/controllers/feed.js
+++ rest-app/backend/controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
-		posts: [{ title: "1st Post", content: "This is a very first dummy post" }],
+		posts: [
+			{
+				_id: "1",
+				title: "1st Post",
+				content: "This is a very first dummy post",
+				imageUrl: "images/dummy.png",
+				creator: {
+					name: "Abhi",
+				},
+				createdAt: new Date(),
+			},
+		],
 	});
 };
 
@@ -9,6 +20,14 @@ exports.createPost = (req, res, next) => {
 	const content = req.body.content;
 	res.status(201).json({
 		messsage: "Create operation successful",
-		post: { id: new Date().toISOString(), title: title, content: content },
+		post: {
+			id: new Date().toISOString(),
+			title: title,
+			content: content,
+			creator: {
+				name: "Abhi",
+			},
+			createdAt: new Date(),
+		},
 	});
 };
