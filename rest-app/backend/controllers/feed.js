fea0f247711247ccfaf765aeb4606d24e3ac4a34
Abhishek Bhavsar <abhi.n.bhavsar@gmail.com>
2020-08-23 11:21:41 +0530

    added connection uri

--- rest-app/backend/controllers/feed.js
+++ rest-app/backend/controllers/feed.js
@@ -1,41 +1,49 @@
-const { validationResult } = require("express-validator");
+const { validationResult } = require('express-validator/check');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-	res.status(200).json({
-		posts: [
-			{
-				_id: "1",
-				title: "1st Post",
-				content: "This is a very first dummy post",
-				imageUrl: "images/dummy.png",
-				creator: {
-					name: "Abhi",
-				},
-				createdAt: new Date(),
-			},
-		],
-	});
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Maximilian'
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
 };
 
 exports.createPost = (req, res, next) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).json({
-			messsage: "Validation failed!",
-			errors: errors.array(),
-		});
-	}
-	const title = req.body.title;
-	const content = req.body.content;
-	res.status(201).json({
-		messsage: "Create operation successful",
-		post: {
-			id: new Date().toISOString(),
-			title: title,
-			content: content,
-			creator: {
-				name: "Abhi",
-			},
-			createdAt: new Date(),
-		},
-	});
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array()
+    });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Maximilian' }
+  });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
